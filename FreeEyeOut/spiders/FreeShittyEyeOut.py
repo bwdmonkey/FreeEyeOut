5dda4437a6b10ddd54461aaabb706d6631406bae
leesw98 <win981026@gmail.com>
2017-07-10 11:36:35 +0900

    Variable Name Change && More Eyes Out

--- FreeEyeOut/spiders/FreeShittyEyeOut.py
+++ FreeEyeOut/spiders/FreeShittyEyeOut.py
@@ -18,22 +18,32 @@ class FreeshittyeyeoutSpider(scrapy.Spider):
         'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=210&section=201',
         'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=210&section=202',
         'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=210&section=203',
-        # 'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=406&section=2W1',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CHEM&course=121&section=102',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CHEM&course=121&section=110',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CHEM&course=121&section=111',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CHEM&course=121&section=122',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CHEM&course=121&section=133',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CHEM&course=121&section=188',
     ]
     def parse(self, response):
         title = response.css("h4::text").extract_first()
         seats = response.css('table.\\27table').extract_first()
         seats = re.split("\D",seats)
         seats = filter(None, seats)
+        total_seats = seats[2]
+        registered_seats = seats[4]
+        general_seats = seats[6]
+        restricted_seats = seats[8]
         print "----------------FreeShittyEyeOut by /u/leesw----------------"
         print "Course: ", title
-        print "Total Seats Remaining: ",seats[2]
-        print "Currently Registered: ",seats[4]
-        print "General Seats Remaining: ",seats[6]
-        print "Restricted Seats Remaining: ",seats[8]
-        open_seats = bool(0 < int(seats[6]))
+        print "Total Seats Remaining: ",total_seats
+        print "Currently Registered: ",registered_seats
+        print "General Seats Remaining: ",general_seats
+        print "Restricted Seats Remaining: ",restricted_seats
+        # Change the seat types accordingly
+        open_seats = bool(0 < int(general_seats))
         if open_seats:
             while open_seats:
                 command = 'say "%s has free seats!"'%title
                 os.system(command)
-                print('\a\a\a')
+                print('%s has free seats!')%title
