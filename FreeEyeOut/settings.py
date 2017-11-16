06256fd55a4f9514f03cd255db2e55bc6f948f59
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2017-11-15 22:33:59 -0800

    Original Version

--- FreeEyeOut/settings.py
+++ FreeEyeOut/settings.py
@@ -14,30 +14,30 @@ BOT_NAME = 'FreeEyeOut'
 SPIDER_MODULES = ['FreeEyeOut.spiders']
 NEWSPIDER_MODULE = 'FreeEyeOut.spiders'
 
-LOG_ENABLED = False;
+LOG_ENABLED=False
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'FreeEyeOut'
+#USER_AGENT = 'FreeEyeOut (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+#CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+#DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED = False
+#COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-TELNETCONSOLE_ENABLED = False
+#TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
@@ -80,7 +80,7 @@ TELNETCONSOLE_ENABLED = False
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- FreeEyeOut/spiders/FreeShittyEyeOut.py
+++ FreeEyeOut/spiders/FreeShittyEyeOut.py
@@ -2,41 +2,39 @@
 import scrapy
 import re
 import os
-import csv
 
-# while true; clear && printf '\e[3J'; date; do scrapy runspider FreeShittyEyeOut.py; sleep 30; done
 
 class FreeshittyeyeoutSpider(scrapy.Spider):
     name = 'FreeShittyEyeOut'
     allowed_domains = ['courses.students.ubc.ca']
-
-    with open('watchlist.csv', 'rb') as f:
-        reader = csv.reader(f)
-        array_list = list(reader)
-
-    start_urls = []
-    for value in array_list:
-        start_urls.append(value[0])
-
+    start_urls = [
+    # EDIT HERE USE THE SIMILAR FORMAT
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=110&section=101',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=110&section=103',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=110&section=104',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=121&section=201',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=121&section=202',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=121&section=203',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=210&section=201',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=210&section=202',
+        'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=CPSC&course=210&section=203',
+    ]
     def parse(self, response):
         title = response.css("h4::text").extract_first()
         seats = response.css('table.\\27table').extract_first()
         seats = re.split("\D",seats)
         seats = filter(None, seats)
-        total_seats = seats[2]
-        registered_seats = seats[4]
-        general_seats = seats[6]
-        restricted_seats = seats[8]
         print "----------------FreeShittyEyeOut by /u/leesw----------------"
         print "Course: ", title
-        print "Total Seats Remaining: ",total_seats
-        print "Currently Registered: ",registered_seats
-        print "General Seats Remaining: ",general_seats
-        print "Restricted Seats Remaining: ",restricted_seats
-        # Change the seat types accordingly
-        open_seats = bool(0 < int(general_seats))
+        print "Total Seats Remaining: ",seats[2]
+        print "Currently Registered: ",seats[4]
+        print "General Seats Remaining: ",seats[6]
+        print "Restricted Seats Remaining: ",seats[8]
+        open_seats = bool(0 < int(seats[6]))
         if open_seats:
             while open_seats:
                 command = 'say "%s has free seats!"'%title
                 os.system(command)
-                print('%s has free seats!')%title
+                print seats[6]
+                print open_seats
+                print('\a\a\a')
