d38a25ca3aae5d587c837d5a4dc418e1f94c40f4
Tom Lee <win981026@gmail.com>
2018-09-04 00:19:59 -0700

    Prettier print in console

--- FreeEyeOut/spiders/services/mailer.py
+++ FreeEyeOut/spiders/services/mailer.py
@@ -10,12 +10,13 @@ class Mailer():
 
 	def alert(self):
 		FROM = config("EMAIL_HOST_USER")
-		TO = config("TEMP_TO_EMAIL", cast=Csv())
+		TO_LIST = config("TEMP_TO_EMAIL", cast=Csv())
+		TO_STRING = ", ".join(TO_LIST)
 		SUBJECT = "%s has free seats!" % self.course
 		TEXT = "%s\n\n%s" % (SUBJECT, self.url)
 
 		message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-		""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+		""" % (FROM, TO_STRING, SUBJECT, TEXT)
 		try:
 			email_host = config("EMAIL_HOST", default="smtp.gmail.com")
 			email_host_user = config("EMAIL_HOST_USER")
@@ -25,8 +26,8 @@ class Mailer():
 			server.ehlo()
 			server.starttls()
 			server.login(email_host_user, email_host_pwd)
-			server.sendmail(FROM, TO, message)
+			server.sendmail(FROM, TO_LIST, message)
 			server.close()
-			print "Successfully sent the mail to %s about %s" % (TO, self.course)
+			print "Successfully sent the mail to %s about %s" % (TO_STRING, self.course)
 		except:
-			print "Failed to send mail to %s about %s" % (TO, self.course)
+			print "Failed to send mail to %s about %s" % (TO_STRING, self.course)
